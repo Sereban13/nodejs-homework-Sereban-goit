c59ea6265e01a37e84aec69695e0a5394cca997d
Sehii Perevertniuk <s.perevertnyuk@gmail.com>
2023-11-05 15:05:00 +0100

    delete coments

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,10 @@
 const express = require("express");
-// const Joi = require("joi");
 //
 const contacts = require("../../models/contacts");
 const { HttpError } = require("../../helpers");
 const contactSchema = require("../../utils/validation/contactValidationSchema");
 
 const router = express.Router();
-
-// const contactSchema = Joi.object({
-//   name: Joi.string().required(),
-//   email: Joi.string()
-//     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-//     .required(),
-//   phone: Joi.string()
-//     .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
-//     .required(),
-// });
 
 router.get("/", async (req, res, next) => {
   try {
@@ -32,12 +21,6 @@ router.get("/:contactId", async (req, res, next) => {
     const result = await contacts.getContactById(contactId);
     if (!result) {
       throw HttpError(404, "Contact not found");
-
-      // const error = new Error("Contact not found");
-      // error.status = 404;
-      // throw error;
-
-      // return res.status(404).json({ message: "Contact not found" });
     }
     res.json(result);
   } catch (error) {
@@ -45,23 +28,6 @@ router.get("/:contactId", async (req, res, next) => {
     // шукає обробник помилок, де є 4 параметри (у нас це знаходиться в арр = app.use);
   }
 });
-
-// router.post("/", async (req, res, next) => {
-//   try {
-//     const { error } = contactSchema.validate(req.body);
-//     if (error) {
-//       const missingKeys = error.details.map((detail) => detail.context.key);
-//       return res.status(404).json({ message: `missing required ${missingKeys} field` });
-
-//     }
-//     const result = await contacts.addContact(req.body);
-//     res.status(201).json(result);
-
-//     console.log(req.body);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
 
 router.post("/", async (req, res, next) => {
   try {
