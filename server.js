3530fab392d59b3e966b66da0f77535e9481013b
Sehii Perevertniuk <s.perevertnyuk@gmail.com>
2023-11-11 19:47:58 +0100

    new branch

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+// mongobd user data:
+// login: Sereban, password: mENpfF0l3Ez30PhF
+
+const mongoose = require("mongoose");
 const app = require("./app");
 
-app.listen(8000, () => {
-  console.log("Server running. Use our API on port: 8000");
-});
+const DbHost =
+  "mongodb+srv://Sereban:mENpfF0l3Ez30PhF@cluster0.vkbil8p.mongodb.net/contacts_manager?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DbHost)
+  .then(() => {
+    app.listen(8000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
